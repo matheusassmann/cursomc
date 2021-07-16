4d0de71e906ccc9496ae2425fc748043c87950f5
Matheus Assmann <matheusassmann@hotmail.com>
2021-07-16 11:51:54 -0300

    Ajusting endpoint /categorias/{id}

--- src/main/java/com/matheusassmann/cursomc/domain/Categoria.java
+++ src/main/java/com/matheusassmann/cursomc/domain/Categoria.java
@@ -9,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Categoria implements Serializable {
 
@@ -19,6 +21,7 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 
+	@JsonManagedReference
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 
--- src/main/java/com/matheusassmann/cursomc/services/CategoriaService.java
+++ src/main/java/com/matheusassmann/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.matheusassmann.cursomc.domain.Categoria;
 import com.matheusassmann.cursomc.repositories.CategoriaRepository;
+import com.matheusassmann.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + obj.getClass().getName()));
 	}
 }
